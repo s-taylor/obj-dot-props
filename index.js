dbeabfbcbcc3d8fa94e00d704c5f8959f386a1be
Simon Taylor <nizmox@gmail.com>
2018-08-29 16:45:30 +1000

    first pass at mapProps method

--- index.js
+++ index.js
@@ -1,23 +1,84 @@
+const cloneDeep = require('lodash.clonedeep');
+const get = require('lodash.get');
+const set = require('lodash.set');
+const orderBy = require('lodash.orderby');
+
+const TRIM = /\]$/; // to trim trailing square bracket
+const SPLIT_RE = /\.|\[|\]\.*/;
 const flatMap = (a, cb) => [].concat(...a.map(cb));
 
-const dotProps = (obj, prefix) => {
-  if (typeof obj !== 'object') throw new Error('Invalid argument, must be an object');
+const sort = arr => orderBy(arr, ['depth', 'fullKey'], ['desc', 'asc']);
 
-  return flatMap(Object.keys(obj), (key) => {
-    const value = obj[key];
-    const fullKey = prefix ? `${prefix}.${key}` : key;
+// const depthDesc = (a, b) => (a.depth < b.depth ? 1 : a.depth > b.depth ? -1 : 0);
 
-    if (Array.isArray(value)) {
-      return flatMap(value, (v, i) => {
-        const arrKey = `${fullKey}[${i}]`;
-        if (typeof v === 'object') return dotProps(v, arrKey);
-        return arrKey;
-      });
+exports.UNSET = '$UNSET_PROPERTY$';
+
+exports.deletePath = (obj, fullPath) => {
+  const pathArr = fullPath.replace(TRIM, '').split(SPLIT_RE);
+
+  let target = obj;
+  for (let i = 0; i < pathArr.length; i += 1) {
+    const path = pathArr[i];
+
+    if (i === pathArr.length - 1) {
+      if (Array.isArray(target)) {
+        const index = parseInt(path, 10);
+        target.splice(index, 1);
+      } else delete target[path];
     }
+    target = target[path];
+  }
 
-    if (typeof value === 'object') return dotProps(value, fullKey);
-    return fullKey;
-  });
+  return obj;
 };
 
-module.exports = dotProps;
+
+const getProps = (options = {}) => {
+  const { parents = false } = options;
+
+  const getter = (obj, prefix = '', depth = 1) => {
+    if (typeof obj !== 'object') throw new Error('Invalid argument, must be an object');
+
+    return flatMap(Object.keys(obj), (key) => {
+      const value = obj[key];
+      const fullKey = prefix ? `${prefix}.${key}` : key;
+      const thisKey = { fullKey, depth };
+
+      if (Array.isArray(value)) {
+        return flatMap(value, (v, i) => {
+          const arrKey = `${fullKey}[${i}]`;
+          if (typeof v === 'object') {
+            return getter(v, arrKey, depth + 2);
+          }
+          return { fullKey: arrKey, depth: depth + 1 };
+        }).concat(parents ? thisKey : []);
+      }
+
+      if (typeof value === 'object') {
+        return getter(value, fullKey, depth + 1)
+          .concat(parents ? thisKey : []);
+      }
+
+      return thisKey;
+    });
+  };
+  return getter;
+};
+
+exports.getProps = (obj, options = {}) => {
+  const getter = getProps(options);
+  const results = getter(obj);
+  return sort(results).map(r => r.fullKey);
+};
+
+exports.mapProps = (obj, predicate, options = {}) => {
+  const result = cloneDeep(obj); // set/unset mutates, so clone it
+  const paths = exports.getProps(result, options);
+  paths.forEach((path) => {
+    const value = get(obj, path);
+    const newValue = predicate(value, path, obj);
+    if (newValue === exports.UNSET) exports.deletePath(result, path);
+    else if (value !== newValue) set(result, path, newValue);
+  });
+  return result;
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,39 @@
 const test = require('ava');
 const get = require('lodash.get');
 const cloneDeep = require('lodash.clonedeep');
-const getDotProps = require('../index');
+const {
+  deletePath, getProps, mapProps, UNSET,
+} = require('../index');
+
+/* deletePath */
+
+test('it must delete object path', (t) => {
+  const obj = { a: { b: { c: [{ d: 1 }] } } };
+  const target = 'a.b.c[0].d';
+
+  const result = deletePath(obj, target);
+
+  const expected = { a: { b: { c: [{}] } } };
+  t.deepEqual(result, expected);
+});
+
+test('it must delete (splice) array path', (t) => {
+  const obj = { a: { b: { c: [0, 1, 2] } } };
+  const target = 'a.b.c[1]';
+
+  const result = deletePath(obj, target);
+
+  const expected = { a: { b: { c: [0, 2] } } };
+  t.deepEqual(result, expected);
+});
+
+/* getProps */
 
 test('it must not mutate the object', (t) => {
   const obj = { a: 1, b: [2, 3], c: { d: 4 } };
   const original = cloneDeep(obj);
 
-  getDotProps(obj);
+  getProps(obj);
 
   t.deepEqual(obj, original);
 });
@@ -15,8 +41,8 @@ test('it must not mutate the object', (t) => {
 test('it must return expected format', (t) => {
   const obj = { a: 1, b: [2, 3], c: { d: 4 } };
 
-  const result = getDotProps(obj);
-  const expected = ['a', 'b[0]', 'b[1]', 'c.d'];
+  const result = getProps(obj, { parents: true });
+  const expected = ['b[0]', 'b[1]', 'c.d', 'a', 'b', 'c'];
 
   t.deepEqual(result, expected);
 });
@@ -24,14 +50,14 @@ test('it must return expected format', (t) => {
 test('it works on deeply nested properies also', (t) => {
   const obj = { a: [{ b: { c: 2 } }], d: { e: { f: [2] } } };
 
-  const result = getDotProps(obj);
-  const expected = ['a[0].b.c', 'd.e.f[0]'];
+  const result = getProps(obj, { parents: true });
+  const expected = ['a[0].b.c', 'd.e.f[0]', 'a[0].b', 'd.e.f', 'd.e', 'a', 'd'];
 
   t.deepEqual(result, expected);
 });
 
 test('it must return an empty array when given an empty object', (t) => {
-  const result = getDotProps({});
+  const result = getProps({});
 
   t.deepEqual(result, []);
 });
@@ -39,15 +65,97 @@ test('it must return an empty array when given an empty object', (t) => {
 test('it must be compatible with lodash.get', (t) => {
   const obj = { a: 1, b: [2, 3], c: { d: 4 } };
 
-  const props = getDotProps(obj);
-  const result = props.map(prop => get(obj, prop));
+  const props = getProps(obj);
+  const result = props.map(prop => get(obj, prop)).sort();
 
   const expected = [1, 2, 3, 4];
   t.deepEqual(result, expected);
 });
 
 test('it must throw when not given an object', (t) => {
-  const error = t.throws(() => getDotProps('a string'));
+  const error = t.throws(() => getProps('a string'));
 
   t.is(error.message, 'Invalid argument, must be an object');
+});
+
+/* mapProps */
+
+test.skip('it must not mutate the object', (t) => {
+  // use all three options to ensure this
+});
+
+test('it must allow updating props', (t) => {
+  const obj = { a: 1, b: [2, 3], c: { d: 4 } };
+
+  const result = mapProps(obj, value => value * 2);
+
+  const expected = { a: 2, b: [4, 6], c: { d: 8 } };
+  t.deepEqual(result, expected);
+});
+
+test('it must not change anything if value returned', (t) => {
+  const obj = { a: 1, b: [2, 3], c: { d: 4 } };
+
+  const result = mapProps(obj, value => value);
+
+  t.deepEqual(result, obj);
+});
+
+test('it must allow unsetting object keys', (t) => {
+  const obj = { a: 1, b: [2, 3], c: { d: 4 } };
+
+  const result = mapProps(obj, (value, pathName) => {
+    if (value === 1) return UNSET;
+    if (pathName === 'c.d') return UNSET;
+    return value;
+  });
+
+  const expected = { b: [2, 3], c: { } };
+
+  t.deepEqual(result, expected);
+});
+
+
+test('it can be used to clean up empty objects', (t) => {
+  const obj = { b: [1, 2], c: {}, d: { e: 3 } };
+
+  const result = mapProps(obj, (value, path) => {
+    if (typeof value === 'object' && Object.keys(value).length === 0) return UNSET;
+    return value;
+  }, { parents: true });
+
+  const expected = { b: [1, 2], d: { e: 3 } };
+
+  t.deepEqual(result, expected);
+});
+
+test('it must allow unsetting array elements', (t) => {
+  const obj = {
+    a: 1, b: [2, 3], c: { d: 4 }, e: [5],
+  };
+
+  const result = mapProps(obj, (value, pathName) => {
+    if (value === 2) return UNSET;
+    if (pathName === 'e[0]') return UNSET;
+    return value;
+  });
+
+  const expected = {
+    a: 1, b: [3], c: { d: 4 }, e: [],
+  };
+
+  t.deepEqual(result, expected);
+});
+
+test('it can be used to clean up empty arrays', (t) => {
+  const obj = { b: [1, 2], c: [], d: { e: 3 } };
+
+  const result = mapProps(obj, (value) => {
+    if (Array.isArray(value) && value.length === 0) return UNSET;
+    return value;
+  }, { parents: true });
+
+  const expected = { b: [1, 2], d: { e: 3 } };
+
+  t.deepEqual(result, expected);
 });
